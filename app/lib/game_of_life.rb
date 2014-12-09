7df38a2c0f6d72cc28fbf7b754e373699802105b
nrakochy <nick.rakochy@gmail.com>
2014-12-09 15:08:31 -0600

    Refactored game_of_life_spec from before :each to let statements

--- app/lib/game_of_life.rb
+++ app/lib/game_of_life.rb
@@ -1,19 +1,19 @@
+require 'pry'
 require 'opal'
 require 'opal-jquery'
 require_relative 'living_cell_coordinate'
 require_relative 'cell_community_rules'
 
 class World
-  attr_reader :living_world
+  attr_reader :living_world, :cell_community_rules
 
-  def create_empty_world
-    @living_world = []
+  def initialize(living_world_coordinates)
+    @living_world = create_community_of_living_cells(living_world_coordinates)
+    @cell_community_rules = CellCommunityRules.new(@living_world)
   end
 
-  def introduce_life_into_the_world(living_cell_coordinate)
-    living_cell = create_living_cell(living_cell_coordinate)
+  def add_living_cell_to_living_world(living_cell)
     @living_world << living_cell
-    living_cell
   end
 
   def tick_to_next_generation_of_life
@@ -24,21 +24,34 @@ class World
   end
 
   def bring_to_life_all_eligible_neighbors
-    dead_eligible_for_life = CellCommunityRules.find_dead_neighbors_eligible_for_life(@living_world)
-    dead_eligible_for_life.each{|dead_cell| @living_cells << create_living_cell(dead_cell)}
+    dead_eligible_for_life = @cell_community_rules.find_dead_neighbors_eligible_for_life
+    dead_eligible_for_life.each do |dead_cell|
+      new_cell = create_living_cell(dead_cell)
+      @living_world << new_cell
+    end
+    @living_world
   end
 
 
   def cell_lives_another_generation?(living_cell)
-    CellCommunityRules.cell_lives_another_generation?(living_cell, @living_cells)
+    @cell_community_rules.cell_lives_another_generation?(living_cell)
   end
 
   def dead_cell_comes_to_life?(dead_cell)
-    CellCommunityRules.dead_cell_comes_to_life?(living_neighbor_count, @living_cells)
+    @cell_community_rules.dead_cell_comes_to_life?(living_neighbor_count, @living_cells)
   end
 
   def create_living_cell(cell_coordinate)
     LivingCellCoordinate.new(cell_coordinate)
+  end
+
+  def create_community_of_living_cells(living_cell_community)
+    community = []
+    living_cell_community.each do |cell_coordinate|
+      new_cell = create_living_cell(cell_coordinate)
+      community << new_cell
+    end
+    community
   end
 end
 
--- app/spec/game_of_life_spec.rb
+++ app/spec/game_of_life_spec.rb
@@ -1,78 +1,72 @@
 require_relative '../lib/game_of_life'
+require_relative '../lib/individual_cell_rules_for_life'
+require_relative '../lib/cell_community_rules'
 
 describe World do
+  let(:cell1){ [ 1, 1 ] }
+  let(:cell2){ [ 2, 1 ] }
+  let(:cell3){ [ 1, 2 ] }
+  let(:cell4){ [ 3, 1 ] }
+  let(:world_with_two_cells){ World.new([cell1, cell2])}
+  let(:world_with_three_cells){ World.new([cell1, cell2, cell3]) }
+  let(:world_with_four_cells){ World.new([cell1, cell2, cell3, cell4]) }
 
-  before(:each) do
-    new_world = World.new
-    new_world.create_empty_world
-    @new_world = new_world
-  end
-
-  describe '#create_empty_world' do
-    it 'creates an empty living world' do
-      expect(@new_world.living_world.empty?).to be(true)
-    end
-  end
-
-  describe '#introduce_life_into_the_world' do
-    it 'adds living_cell_coordinate Object to the living world' do
-      @new_world.introduce_life_into_the_world([1,1])
-      expect(@new_world.living_world.first.class).to eq(LivingCellCoordinate)
-      expect(@new_world.living_world.count).to eq(1)
+  describe '#create_community_of_living_cells' do
+    it 'adds living_cell Object to the living world' do
+      new_world = World.new([ [ 1, 1 ] ])
+      expect(new_world.living_world.first.class).to eq(LivingCellCoordinate)
+      expect(new_world.living_world.count).to eq(1)
     end
   end
 
   describe 'tick_to_next_generation_of_life' do
-    it 'keeps 1 living_cell of 3 which should live to the next generation' do
-      first_cell = @new_world.introduce_life_into_the_world([1,1])
-      second_cell = @new_world.introduce_life_into_the_world([2,1])
-      last_cell = @new_world.introduce_life_into_the_world([0,1])
-      expect(@new_world.tick_to_next_generation_of_life).to include(first_cell)
-      expect(@new_world.tick_to_next_generation_of_life).not_to include(second_cell, last_cell)
+    context '2 living cells or less' do
+      it 'removes all cells, given there is only 2 living cells in the World' do
+        expect(world_with_two_cells.tick_to_next_generation_of_life.count).to eq(0)
+      end
     end
 
-    it 'keeps 3 living_cells of 4 which should live to the next generation' do
-      first_cell = @new_world.introduce_life_into_the_world([1,1])
-      second_cell = @new_world.introduce_life_into_the_world([2,1])
-      third_cell = @new_world.introduce_life_into_the_world([1,0])
-      last_cell = @new_world.introduce_life_into_the_world([8,1])
-      expect(@new_world.tick_to_next_generation_of_life).to include(first_cell, second_cell, third_cell)
-      expect(@new_world.tick_to_next_generation_of_life).not_to include(last_cell)
+    context '3 living cells' do
+      it 'keeps 3 living cells and adds 1 dead cell to the living world' do
+        expect(world_with_three_cells.tick_to_next_generation_of_life.count).to eq(4)
+      end
     end
 
-    it 'gives life to a dead cell with 3 living neighbors' do
-      first_cell = @new_world.introduce_life_into_the_world([1,1])
-      second_cell = @new_world.introduce_life_into_the_world([1,2])
-      third_cell = @new_world.introduce_life_into_the_world([2,2])
-      last_cell = @new_world.introduce_life_into_the_world([8,1])
-      @new_world.tick_to_next_generation_of_life
-      expect(@new_world.living_world).not_to include(last_cell)
+    xit 'keeps 3 living_cells of 4 which should live to the next generation' do
+      expect(world_with_four_cells.tick_to_next_generation_of_life.count).to eq(3)
+    end
+
+    xit 'gives life to a dead cell with 3 living neighbors' do
+      expect(new_world.living_world).not_to include(last_cell)
     end
   end
 
   describe '#create_living_cell' do
     it 'creates a living_cell_coordinate Object' do
-      expect(@new_world.create_living_cell([1,1]).class).to eq(LivingCellCoordinate)
+      expect(new_world.create_living_cell([1,1]).class).to eq(LivingCellCoordinate)
     end
   end
 
     describe '#bring_to_life_all_eligible_neighbors' do
     it 'returns an empty array if no dead cells come to life' do
-      first_cell = @new_world.introduce_life_into_the_world([1,1])
-      expect(@new_world.bring_to_life_all_eligible_neighbors.empty?).to eq(true)
+      first_cell = new_world.introduce_life_into_the_world([1,1])
+      expect(new_world.bring_to_life_all_eligible_neighbors.empty?).to eq(true)
     end
 
     it 'creates life for 1 dead cell with 3 living neighbors' do
-      first_cell = @new_world.introduce_life_into_the_world([1,1])
-      second_cell = @new_world.introduce_life_into_the_world([1,2])
-      third_cell = @new_world.introduce_life_into_the_world([2,2])
-      expect(@new_world.bring_to_life_all_eligible_neighbors.count).to eq(1)
-      expect(@new_world.bring_to_life_all_eligible_neighbors.first.class).to eq(LivingCellCoordinate)
-      expect(@new_world.bring_to_life_all_eligible_neighbors.first.find_living_coordinate).to eq([2,1])
+      first_cell = new_world.introduce_life_into_the_world([1,1])
+      second_cell = new_world.introduce_life_into_the_world([1,2])
+      third_cell = new_world.introduce_life_into_the_world([2,2])
+      expect(new_world.bring_to_life_all_eligible_neighbors.count).to eq(1)
+      expect(new_world.bring_to_life_all_eligible_neighbors.first.class).to eq(LivingCellCoordinate)
+      expect(new_world.bring_to_life_all_eligible_neighbors.first.find_living_coordinate).to eq([2,1])
     end
   end
 end
 
+def find_cell_coordinate(cell)
+  cell.find_living_coordinate
+end
 
 
 
